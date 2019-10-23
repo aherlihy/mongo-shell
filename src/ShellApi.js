95f73f1abae8a4f08538ea8bcdbceb7f19c6673d
aherlihy <anna.herlihy@10gen.com>
2019-10-23 18:37:57 +0200

    Add generated file

---
+++ src/ShellApi.js
@@ -0,0 +1,72 @@
+class ShellApi {
+  constructor(mapper) {
+    this.mapper = mapper;
+    this.help = "Welcome to the new MongoDB Shell!\nAttributes: customCmd, use"
+    this.customCmd = function() {
+      return this.mapper.customCmd(...arguments);
+    };
+    this.customCmd.help = "default help\nAttributes: serverVersions, topologies";
+    this.customCmd.serverVersions = [0,-1];
+    this.customCmd.topologies = ["ReplSet","Standalone"];
+    this.use = function() {
+      return this.mapper.use(...arguments);
+    };
+    this.use.help = "default help\nAttributes: serverVersions, topologies";
+    this.use.serverVersions = [0,-1];
+    this.use.topologies = ["ReplSet","Standalone"];
+  }
+}
+class Collection {
+  constructor(mapper, database, collection) {
+    this.mapper = mapper;
+    this.database = database;
+    this.collection = collection;
+    this.help = "The collection class.\nAttributes: find"
+    this.find = function() {
+      return this.mapper.find(...arguments);
+    };
+    this.find.help = "default help\nAttributes: serverVersions, topologies";
+    this.find.serverVersions = [0,-1];
+    this.find.topologies = ["ReplSet","Standalone"];
+  }
+}
+class Cursor {
+  constructor(mapper) {
+    this.mapper = mapper;
+    this.help = "The cursor class.\nAttributes: next"
+    this.next = function() {
+      return this.mapper.next(...arguments);
+    };
+    this.next.help = "default help\nAttributes: serverVersions, topologies";
+    this.next.serverVersions = [0,-1];
+    this.next.topologies = ["ReplSet","Standalone"];
+  }
+}
+class Database {
+  constructor(mapper, database) {
+    this.mapper = mapper;
+    this.database = database;
+    this.help = "The database class.\nAttributes: runCommand"
+    this.runCommand = function() {
+      return this.mapper.runCommand(...arguments);
+    };
+    this.runCommand.help = "Runs an arbitrary command on the database.\nAttributes: serverVersions, topologies";
+    this.runCommand.serverVersions = [0,-1];
+    this.runCommand.topologies = ["ReplSet","Standalone"];
+  }
+}
+class ReplicaSet {
+  constructor(mapper) {
+    this.mapper = mapper;
+    this.help = "The Replica Set class.\nAttributes: "
+  }
+}
+class Shard {
+  constructor(mapper) {
+    this.mapper = mapper;
+    this.help = "The shard class.\nAttributes: "
+  }
+}
+
+export default ShellApi;
+export { ShellApi, Collection, Cursor, Database, ReplicaSet, Shard };
